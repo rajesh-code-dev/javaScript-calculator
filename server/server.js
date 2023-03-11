c20b4ac8b7d79cf04676069cf32cbadf53319532
Rajesh <choudharyrajesh729@gmail.com>
2023-03-11 18:06:51 +0530

    pu

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic('', { index: ['calculator.html', 'calculator.htm'] })
+
+// Create server
+var server = http.createServer(function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+})
+
+// Listen
+server.listen(3000)
